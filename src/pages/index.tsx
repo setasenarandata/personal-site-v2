587dda7aaebb37173bfebea1826f9c1bae256440
setasenarandata <setasena93@gmail.com>
2023-06-07 22:47:24 +0700

    fix favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,13 @@ const Home: NextPage = () => {
         <Headers />
       </div>
 
-      <div className="md:px-24 px-12 py-12 md:py-24 lg:px-48">
+      <div className="md:px-24 px-12 pt-12 md:pt-24 pb-12 lg:px-48">
         <p className="text-[#1A1A1A] text-3xl md:text-5xl lg:text-6xl font-semibold leading-relaxed">Setasena Randata is a Software Engineer living and working in Jakarta.</p>
-        <p className="mt-12 text-[#1A1A1A] text-lg md:text-xl lg:text-2xl font-medium leading-relaxed">Currently Android Developer at Bangkit by Google, GoTo & Traveloka. Still an undergraduate student of computer science at University of Indonesia.</p>
+        <p className="mt-12 text-[#1A1A1A] text-lg md:text-xl lg:text-2xl font-medium leading-relaxed">Currently Android Developer at <span className="underline"><a href="https://grow.google/intl/id_id/bangkit">Bangkit by Google, GoTo & Traveloka.</a></span> Still an undergraduate student of computer science at <span className="underline"><a href="https://ui.ac.id">University of Indonesia.</a></span></p>
       </div>
 
       <div className="md:px-24 px-12 lg:px-48">
         <Experience />
-
       </div>
 
 
