659093c41dde315cb9b36c44daf63f51c771783e
setasenarandata <setasena93@gmail.com>
2023-06-18 02:10:45 +0700

    add relational model

--- src/server/api/routers/experience.ts
+++ src/server/api/routers/experience.ts
@@ -4,11 +4,14 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const experienceRouter = createTRPCRouter({
   getAll: publicProcedure.query( async ({ ctx }) => {
-    const experience = await ctx.prisma.experience.findMany()
+    const experience = await ctx.prisma.experience.findMany({
+      include: { stacks: true, media: true }
+    })
     return experience
   }),
   getWorks: publicProcedure.query( async ({ ctx }) => {
     const works = await ctx.prisma.experience.findMany({
+      include: { stacks: true, media: true },
       where: {
         type: "WORK"
       }
@@ -19,6 +22,7 @@ export const experienceRouter = createTRPCRouter({
 
   getProjects: publicProcedure.query( async ({ ctx }) => {
     const projects = await ctx.prisma.experience.findMany({
+      include: { stacks: true, media: true },
       where: {
         type: "PROJECT"
       }
@@ -29,6 +33,7 @@ export const experienceRouter = createTRPCRouter({
 
   getOrganizations: publicProcedure.query( async ({ ctx }) => {
     const organizations = await ctx.prisma.experience.findMany({
+      include: { stacks: true, media: true },
       where: {
         type: "ORGANIZATION"
       }
