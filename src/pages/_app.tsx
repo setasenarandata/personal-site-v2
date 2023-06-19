1f821e4bab6897b31edc844862111751bc44b4f1
setasenarandata <setasena93@gmail.com>
2023-06-19 14:26:13 +0700

    add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,17 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>Setasena</title>
+        <title>Setasena Randata Ramadanie</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="/images/android-chrome-192x192.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/images/android-chrome-512x512.png" />
+        <meta name="description" content="Currently Android Developer at Bangkit by Google, GoTo & Traveloka. Still an undergraduate student of computer science at University of Indonesia." />
+        <meta name="author" content="Setasena Randata Ramadanie" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="setasena, sena, setasena randata ramadanie, setasenarr, setasena93" />
       </Head>
       <Component {...pageProps} />
     </>
