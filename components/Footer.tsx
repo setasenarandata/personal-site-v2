311ed1540ba437447495c333669f72a4cacc4e4b
setasenarandata <setasena93@gmail.com>
2023-06-29 11:44:39 +0700

    add created by me

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -48,7 +48,7 @@ export default function Footer() {
                 </div>
                 <div className="mt-8 md:order-1 md:mt-0">
                     <p className="text-center text-xs leading-5 text-gray-500">
-                        &copy; 2023 Setasena Randata Ramadanie. All rights reserved.
+                        &copy; 2023 Setasena Randata Ramadanie. Created by <span className="text-blue-900"> <a href="https://setasena.com">Setasena Randata Ramadanie</a></span>. All rights reserved.
                     </p>
                 </div>
             </div>
