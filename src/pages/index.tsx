d7b235bdf506ab13158e8d30e602fc26770bfa90
setasenarandata <setasena93@gmail.com>
2023-07-19 23:58:44 +0700

    update resume

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ const actions = [
     id: 'action-5',
     title: 'Resume',
     icon: <AiOutlineFilePdf size={24} />,
-    link: 'https://drive.google.com/file/d/14WU3t8CUC8r8ROpHWb16EyHcbSOmTZY0/view?usp=sharing',
+    link: 'https://drive.google.com/file/d/1QkURsPnQZOFZ4SKrpjaYfFUy5fpNx-xy/view?usp=sharing',
   },
 
 ]
